058d096ca7a4aa7e5d82dff436c33144f798ee8e
abraham angulo <abraham.angulo.e@gmail.com>
2017-05-02 20:37:12 -0700

    added print hello world method

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by gamec on 5/1/2017.
  */
 public class HelloWorld {
+    public static void main (String [] args) {
+        System.out.println("hello world");
+    }
 }
